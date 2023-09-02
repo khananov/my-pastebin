fffdd2b784199738cd9d7fb6428c3ecaee57b469
khananov <khananovtf@gmail.com>
2023-09-02 07:14:45 +0300

    Create project

---
+++ src/main/java/ru/khananov/mypastebin/MyPastebinApplication.java
@@ -0,0 +1,13 @@
+package ru.khananov.mypastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPastebinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPastebinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/khananov/mypastebin/MyPastebinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khananov.mypastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPastebinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
