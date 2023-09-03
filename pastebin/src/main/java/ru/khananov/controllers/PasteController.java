b540dfdba9c7233dfbfb928b73a9a3dfdb7197bf
khananov <khananovtf@gmail.com>
2023-09-03 20:35:29 +0300

    Create mapper for entity Paste

--- pastebin/src/main/java/ru/khananov/controllers/PasteController.java
+++ pastebin/src/main/java/ru/khananov/controllers/PasteController.java
@@ -1,29 +1,32 @@
 package ru.khananov.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.khananov.entyties.Paste;
 import ru.khananov.entyties.dto.PasteRequestDto;
 import ru.khananov.entyties.dto.PasteResponseDto;
+import ru.khananov.mappers.PasteMapper;
 import ru.khananov.services.PasteService;
 
 @RestController
 @RequestMapping("api/pastebin")
 public class PasteController {
     private final PasteService pasteService;
+    private final PasteMapper pasteMapper;
 
     @Autowired
-    public PasteController(PasteService pasteService) {
+    public PasteController(PasteService pasteService, PasteMapper pasteMapper) {
         this.pasteService = pasteService;
+        this.pasteMapper = pasteMapper;
     }
 
     @PostMapping
     public ResponseEntity<PasteResponseDto> save(@RequestBody PasteRequestDto pasteRequestDto) {
-        return new ResponseEntity<>(pasteService.create(pasteRequestDto), HttpStatus.OK);
+        Paste paste = pasteMapper.toEntity(pasteRequestDto);
+        paste.setHash("hash");
+        System.out.println(paste);
+        return new ResponseEntity<>(pasteMapper.toDto(paste), HttpStatus.OK);
     }
 }
