47158abab68ee67c179aad3f8e400c3f73dc08c4
khananov <khananovtf@gmail.com>
2023-09-10 00:30:29 +0300

    Create feign client for auth servicee

---
+++ authenticate-service/src/main/java/ru/khananov/feignclients/PasteFeignClient.java
@@ -0,0 +1,17 @@
+package ru.khananov.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.khananov.entities.dto.PasteRequestDto;
+import ru.khananov.entities.dto.PasteResponseDto;
+
+@FeignClient(name = "paste-data-service", url = "http://localhost:8765/api/data/pastes")
+public interface PasteFeignClient {
+
+    @GetMapping("/{hash}")
+    ResponseEntity<PasteResponseDto> getByHash(@PathVariable String hash, @RequestParam String email);
+
+    @PostMapping
+    ResponseEntity<PasteResponseDto> save(@RequestBody PasteRequestDto pasteRequestDto, @RequestParam String email);
+}
